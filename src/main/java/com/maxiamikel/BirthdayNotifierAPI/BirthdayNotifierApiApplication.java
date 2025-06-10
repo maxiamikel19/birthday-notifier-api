ec33b0ee4689df7a8cc266b7027ec04e418a5cbd
maxiamikel19 <maxloversist19@gmail.com>
2025-06-10 08:22:28 -0400

    Feat: First commit

---
+++ src/main/java/com/maxiamikel/BirthdayNotifierAPI/BirthdayNotifierApiApplication.java
@@ -0,0 +1,13 @@
+package com.maxiamikel.BirthdayNotifierAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BirthdayNotifierApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BirthdayNotifierApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maxiamikel/BirthdayNotifierAPI/BirthdayNotifierApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maxiamikel.BirthdayNotifierAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BirthdayNotifierApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
