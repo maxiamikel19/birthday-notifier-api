c77cbf237985f9358982d402a49e28d59bba08c0
maxiamikel19 <maxloversist19@gmail.com>
2025-06-10 11:45:35 -0400

    Feat: Implementation of GET: /users/all endpoint

--- src/main/java/com/maxiamikel/BirthdayNotifierAPI/controllers/UserController.java
+++ src/main/java/com/maxiamikel/BirthdayNotifierAPI/controllers/UserController.java
@@ -1,11 +1,16 @@
 package com.maxiamikel.BirthdayNotifierAPI.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.maxiamikel.BirthdayNotifierAPI.dtos.UserRequestDto;
@@ -31,6 +36,16 @@ public class UserController {
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(new ApiResponse(SUCCESS_MESSAGE, HttpStatus.CREATED.getReasonPhrase(), userDto));
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<ApiResponse> findAll(@RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size, @RequestParam(defaultValue = "name") String sortBy) {
+
+        Pageable pageable = PageRequest.of(page, size, Sort.by(sortBy).ascending());
+        var pageUsers = userService.findAll(pageable);
+        var pageUsersDto = pageUsers.map(user -> UserMapper.mapToDto(user));
+        return ResponseEntity.status(HttpStatus.OK).body(new ApiResponse(SUCCESS_MESSAGE, "Found", pageUsersDto));
     }
 
 }
